26b5a7f5dd14357305391574d1506d649a1d0d55
marco-leee <marco.leee@outlook.com>
2025-09-19 12:52:58 +0800

    update: simply workspace

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,5 +1,5 @@
-import { CRYPTO } from '@intellistixman/crypto';
-import { DATA_STRUCTURES, COMBINED } from '@intellistixman/data-structures';
-import { ALGORITHMS } from '@intellistixman/algorithms';
+const main = async () => {
+    // do something
+};
 
-console.log(CRYPTO, DATA_STRUCTURES, ALGORITHMS, COMBINED);
+main();
